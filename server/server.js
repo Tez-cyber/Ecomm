a441fe1bf4e964a75dc90d6769891d67d43bb61a
Salami Azeez <salamitopeazeez@gmail.com>
2025-07-20 15:59:24 +0100

    Added auth route file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
-dotenv.config()
+// Routes import
+import authRoutes from './routes/auth.route.js';
 
+dotenv.config()
 const app = express();
 
+// ======= routes
+app.use("/api/auth", authRoutes)
+
+
+// ==============
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
