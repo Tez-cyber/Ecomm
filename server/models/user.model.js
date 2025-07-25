2495b65345ec5d44ef3037815d2b9d8ce1a2ea13
Salami Azeez <salamitopeazeez@gmail.com>
2025-07-25 03:26:08 +0100

    Hashing password before saving to DB

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,4 +40,17 @@ const userSchema = new mongoose.Schema({
 })
 
 const User = mongoose.model("User", userSchema);
+
+// Pre save hook to hash password
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
 export default User;
