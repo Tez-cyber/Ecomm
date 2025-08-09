bf3419fb63df50bfd24087d790fa15cc1457459e
Salami Azeez <salamitopeazeez@gmail.com>
2025-08-09 21:55:27 +0100

    Added storeRefreshToken function

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken"
+import { redis } from "./redis"
 
-export const generateToken = (userId) => {
+export const generateTokens = (userId) => {
     const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "15m"
     })
@@ -10,4 +11,8 @@ export const generateToken = (userId) => {
     })
 
     return { accessToken, refreshToken }
+}
+
+export const storeRefreshToken = async(userId, refreshToken) => {
+    await redis.set(`refresh_token: ${userId}`, refreshToken, "EX", 7*24*3600); // 7days
 }
